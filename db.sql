472d29fe0006e865d40b47a17e0ee4fc14d23ffa
csandrisTurr <csaszarandras@turr.hu>
2024-10-01 11:13:45 +0200

    fix: sql syntax

--- db.sql
+++ db.sql
@@ -1,33 +1,33 @@
 CREATE TABLE `users` (
-    `id` INT PRIMARY KEY NOT NULL AUTOINCREMENT,
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(32) NOT NULL,
     `email` VARCHAR(64) NOT NULL,
     `password` VARCHAR(40) NOT NULL,
     `role` VARCHAR(3) NOT NULL, -- adm, usr, mod
     `phone` VARCHAR(15),
-    `banned` TINYINT(1) NOT NULL,
+    `banned` TINYINT(1) NOT NULL
 );
 
 CREATE TABLE `recipe` (
-    `id` INT PRIMARY KEY NOT NULL AUTOINCREMENT,
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(64) NOT NULL,
-    `description` STRING NOT NULL,
+    `description` VARCHAR NOT NULL,
     `calories` INT NOT NULL,
     `time` INT NOT NULL, -- adm, usr, mod
-    `image` STRING,
+    `image` VARCHAR
 );
 
 CREATE TABLE `addition` (
-    `recipe_id` INT FOREIGN KEY NOT NULL,
-    `name` VARCHAR(64) NOT NULL,
+    `recipe_id` INT NOT NULL,
+    `name` VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE `category_claim` (
-    `recipe_id` FOREIGN KEY NOT NULL,
-    `category_id` FOREIGN KEY NOT NULL,
-)
+    `recipe_id` INT NOT NULL,
+    `category_id` INT NOT NULL
+);
 
 CREATE TABLE `category` (
-    `id` INT PRIMARY KEY NOT NULL AUTOINCREMENT,
-    `name` VARCHAR(32) NOT NULL,
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL
 );
